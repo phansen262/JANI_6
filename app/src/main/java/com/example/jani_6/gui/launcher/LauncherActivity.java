83cb754e4efd829b001dcf69662bf0ca11791352
Peter Hansen <peterhansen1198@gmail.com>
2023-06-29 18:31:10 -0500

    Test Commit Reinstall

---
+++ app/src/main/java/com/example/jani_6/gui/launcher/LauncherActivity.java
@@ -0,0 +1,8 @@
+package com.example.jani_6.gui.launcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LauncherActivity extends AppCompatActivity {
+
+
+}
---
+++ app/src/main/java/com/example/jani_6/gui/main/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example.jani_6.gui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
