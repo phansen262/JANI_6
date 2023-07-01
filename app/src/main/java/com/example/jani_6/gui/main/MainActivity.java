2bb5a17183e76c8c01850871168bc2f3dd72e4d1
Peter Hansen <peterhansen1198@gmail.com>
2023-07-01 15:04:12 -0500

    Now launches to main activity

--- app/src/main/java/com/example/jani_6/gui/main/MainActivity.java
+++ app/src/main/java/com/example/jani_6/gui/main/MainActivity.java
@@ -1,8 +1,23 @@
 package com.example.jani_6.gui.main;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.jani_6.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
 
+        super.onCreate(savedInstanceState);
+
+        //Initiate layout aspects
+        setContentView(R.layout.activity_main);
+
+
+
+    }
 }
